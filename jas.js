c1eea7356b356925e7ae99f72fca65381c4498cc
cafeom <nguyenhoangnam08031993@gmail.com>
2018-06-04 01:27:07 +0700

    12

--- jas.js
+++ jas.js
@@ -7,6 +7,15 @@ function when_selection_init()
 	{
 		hulk_master = selection;
 	}
+function selection_from_array(arr)
+	{
+		select(arr[0].id);
+
+		for (var i = 1; i < arr.length; i++)
+		{
+			add(arr[i].id);
+		}
+	}
 function change_selection_type(trigger_type)
 	{
 		$('.selectior_row').remove();
@@ -15,19 +24,32 @@ function change_selection_type(trigger_type)
 
 		// create .selection_row elements from near_me array
 
-		selection_row = $('<DIV/>', {class: 'selection_row'}).appendTo('#mini');
-		selection_b4  = $('<DIV/>', {class: 'selection_b4'}).appendTo(selection_row);
-		selection_af  = $('<DIV/>', {class: 'selection_af'}).appendTo(selection_row);
+		selection_row = $('<DIV/>', {type: trigger_type, class: 'selection_row'}).appendTo('#mini');
+		selection_b4  = $('<DIV/>', {type: trigger_type, class: 'selection_b4'}).appendTo(selection_row);
+		selection_af  = $('<DIV/>', {type: trigger_type, class: 'selection_af'}).appendTo(selection_row);
 
 		// so depends on the trigger_type, we will have the equivalent b4 and af
 
-		switch(trigger_type)
+		if ( trigger_type.indexOf('color') >= 0 )
 		{
-			
+			selection_b4.add(selection_af).css('background-color', hulk_master[0].doc.css(trigger_type));
+		}
+		else if ( trigger_type.indexOf('font') >= 0 )
+		{
+			selection_b4.add(selection_af).text(hulk_master[0].doc.css(trigger_type));
 		}
 
-		// btw think about how to quickly replace content (maybe better than what we've got)
-		// just select multiple elements and batch then to replace content
+		selection_row.contextmenu(function(e)
+		{
+			if ( e.ctlKey == true )
+			{
+				selection_from_array(near_me); // you can batch replace content after this step
+			}
+
+			e.preventDefault();
+		})
+
+		// when the #mini panel shows up, we select our row with W/S keys and change the propeties with arrow keys
 	}
 function find_stuffs_near_me(which_type)
 	{
