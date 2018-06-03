60b77e4bf0776e5052adb70e19ce33407c3b24c1
cafeom <nguyenhoangnam08031993@gmail.com>
2018-06-03 22:54:15 +0700

    1

--- jas.js
+++ jas.js
@@ -1,4 +1,7 @@
-$('button').click(function(e)
-{
-	console.log( $(this).text() );
-})
+// when a selection is selected, that selection is indexed to a variable
+// when hit a button, selection will change, with the selection type
+
+function when_selection_init()
+	{
+		
+	}
